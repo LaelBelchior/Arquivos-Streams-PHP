9cad6cd3315cdc44a02e0cc828e5d803b592a615
LaelBelchior <laelbelchior@gmail.com>
2022-04-11 21:35:42 -0300

    Adicionando comentario para criacao de arquivo

--- escritor-clubes.php
+++ escritor-clubes.php
@@ -3,3 +3,7 @@
 $clube = 'Ajax';
 
 file_put_contents('clubes-php.txt', $clube, FILE_APPEND);
+
+/*Para criar um arquivo por aqui, executar a função 
+fopen('nome-arquivo' , 'a') e para adicionar fwrite('nome-arquivo', 'nome-variavel-conteudo')*/
+
