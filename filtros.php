b93ae09e2d1ee7bbe86ede99f2b816c668466b39
LaelBelchior <laelbelchior@gmail.com>
2022-04-12 22:46:06 -0300

    Primeira criacao de filtro para retornar apenas os times de Manchester

--- filtros.php
+++ filtros.php
@@ -1,7 +1,10 @@
 <?php
 
+require 'meuFiltro.php';
+
 $arquivosClubes = fopen('lista-clubes.txt', 'r');
 
-stream_filter_append($arquivosClubes, 'string.toupper');
+stream_filter_register('alura.manchester', MeuFiltro::class);
+stream_filter_append($arquivosClubes, 'alura.manchester');
 
 echo fread($arquivosClubes, filesize('lista-clubes.txt'));
